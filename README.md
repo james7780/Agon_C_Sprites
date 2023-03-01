6ab5c8c13d34b0b9d642a14bfa3fdb8736e51e95
Jum <james7780@yahoo.com>
2023-03-01 09:59:34 -0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Agon_Critters
+ 
---
+++ main.c
@@ -0,0 +1,195 @@
+/*
+ * Title:			Space Critters for Agon Light
+ * Author:			James Higgs (Jum Hig)
+ * Created:			2023
+ * Last Updated:		2023-02-28
+ *
+ * Modinfo:
+ */
+ 
+#include <stdio.h>
+#include <ctype.h>
+#include "mos-interface.h"
+#include "vdp.h"
+
+#include "spritedata.h"
+
+#define BITMAP_WIDTH	16
+#define BITMAP_HEIGHT	16
+
+#define NUM_SPRITES 50
+
+extern int rand();
+extern char getkeycode();
+
+
+typedef struct //SPRITEPOS_t
+{
+	UINT16 x;
+	UINT16 y;
+	INT16 dx;
+	INT16 dy;
+} SPRITEPOS;
+
+SPRITEPOS sp[NUM_SPRITES];
+
+// Get a random number up to R
+int randR(int R)
+{
+	return (rand() % R);
+}
+
+// Initialise the sprite control blocks
+void setupSprites()
+{
+	UINT8 i;
+	for (i = 0; i < NUM_SPRITES; i++)
+		{
+		vdp_spriteSelect(i);
+		vdp_spriteClearFramesSelected();
+		vdp_spriteAddFrameSelected(2);			// UFO frame 1
+		vdp_spriteAddFrameSelected(3);			// UFO frame 2
+		vdp_spriteAddFrameSelected(4);			// UFO frame 3
+		vdp_spriteSetFrameSelected(0);
+		vdp_spriteShowSelected();
+			
+		sp[i].x = randR(300);
+		sp[i].y = randR(190);
+		sp[i].dx = (randR(100) < 50) ? -1 : 1;
+		sp[i].dy = (randR(100) < 50) ? -1 : 1;
+		}
+}
+
+// Wait a specific number of frames (vertical syncs)
+void waitFrames(int numFrames)
+{
+	int i, j;
+	for (i = 0; i < numFrames; i++)
+		{
+		waitvblank();
+		// wait for a bit till we are out of vblank
+		for (j= 0; j < 10000; j++) { }
+		}
+}
+
+/// @param[in] argc			Argument count
+/// @param[in] argv			Pointer to the argument string - zero terminated, parameters separated by spaces
+int main(int argc, char * argv[]) {
+	int	i, j;
+	UINT8 k, flags;
+	UINT16 t;
+	
+	//putch(0x0C);		// CLS
+	//DI();
+	
+	vdp_mode(2);
+	vdp_cursorDisable();
+	vdp_cls();
+	
+	
+	//printf("Hello World\n\r");
+	//printf("Arguments:\n\r");
+	//printf("- argc: %d\n\r", argc);
+	//
+	//for(i = 0; i < argc; i++) {
+	//	printf("- argv[%d]: %s\n\r", i, argv[i]);
+	//}
+	
+	printf("C Sprites Demo\n\r");
+
+	
+	// Upload sprite data to the VDP
+	printf("Uploading bitmap data...\n\r");
+	vdp_bitmapSendData(0, BITMAP_WIDTH, BITMAP_HEIGHT, playerShipData);
+	vdp_bitmapSendData(1, BITMAP_WIDTH, BITMAP_HEIGHT, playerBulletData);
+	vdp_bitmapSendData(2, BITMAP_WIDTH, BITMAP_HEIGHT, ufoData[0]);	
+	vdp_bitmapSendData(3, BITMAP_WIDTH, BITMAP_HEIGHT, ufoData[1]);
+	vdp_bitmapSendData(4, BITMAP_WIDTH, BITMAP_HEIGHT, ufoData[2]);
+	vdp_bitmapSendData(5, BITMAP_WIDTH, BITMAP_HEIGHT, ufoBulletData);
+	
+	for (i = 0; i < 10; i++)
+		{
+		vdp_bitmapDraw(0, randR(300) , randR(160) + 30);
+		}
+	vdp_spriteRefresh();
+	waitFrames(50);
+		
+	// Set up sprites
+	// Note: BBCBASIC procedure:
+	//    1. Select sprite n   (VDU 23, 27, 4, n)
+	//    2. Clear frames for current sprite  (VDU 23, 27, 5)
+	//    3. Add bitmap 0 as frame 0 of current sprite (VDU 23, 27, 6, 0)
+	//    4. Show the current sprite (VDU 23, 27, 11)
+	//    5. Set total number of sprites to display (VDU 23, 27, 7, total)
+	//   Move the sprites:
+	//    1. Select sprite n (VDU 23, 27, 4, n)
+	//    2. Move selected sprite to  (VDU 23, 27, 13, X, Y) 
+	//   Refresh the sprites
+	//    1. *FX19    (wait for VSYNC)
+	//    2. Refresh (VDU 23, 27, 15)
+	
+	printf("Setting up sprites...\n\r");
+	setupSprites();		// sets up 16 sprites
+	
+	vdp_spriteActivateTotal(NUM_SPRITES);
+	
+	for (i = 0; i < NUM_SPRITES; i++)
+		{
+		vdp_spriteMoveTo(i, sp[i].x, sp[i].y);
+		}	
+	
+	vdp_spriteRefresh();
+
+	//EI();
+	
+	printf("Animating... Press any key to exit\n\r");
+	for (i = 0; i < 1000; i++)
+		{
+		waitvblank();
+		if (i > 500)
+			{
+			vdp_scroll(1, 1, 1);
+			vdp_plotColour(randR(256), randR(256), randR(256));
+			vdp_plotPoint(319, randR(200));
+			}
+
+		for (j = 0; j < NUM_SPRITES; j++)
+			{
+			sp[j].x += sp[j].dx;
+			if (sp[j].x < 2)
+				sp[j].dx = 1;
+			else if (sp[j].x > 317)
+				sp[j].dx = -1;
+			
+			sp[j].y += sp[j].dy;
+			if (sp[j].y < 2)
+				sp[j].dy = 1;
+			else if (sp[j].y > 253)
+				sp[j].dy = -1;
+			
+			vdp_spriteSelect(j);
+			vdp_spriteMoveToSelected(sp[j].x, sp[j].y);
+			vdp_spriteNextFrameSelected();
+			}
+		
+		vdp_spriteRefresh();
+			
+		//keycode = getsysvar8bit(sysvar_keycode);	// doesn't work?
+		k = getkeycode();
+		//printf("%d\n\r", k);
+		//if (27 == k)
+		if (k != 0)
+			break;
+
+		//t = getsysvar16bit(sysvar_time);
+		//vdp_cursorGoto(0, 20);
+		//printf("%d\n\r", t);
+		}
+		
+	vdp_spriteActivateTotal(0);
+	vdp_spriteRefresh();
+	vdp_mode(1);
+	vdp_cursorEnable();
+	
+	return 0;
+}
